bf648bb3ff8af999c894faf5f4d193964ba2fe19
Александр Малафеевский <sanho50001@bk.ru>
2023-05-06 00:00:39 +0300

    check services

--- app_catalog/models.py
+++ app_catalog/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+user = get_user_model()
+
+
+class Products(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    goods = models.OneToOneField(Products, on_delete=models.CASCADE, related_name='goods')
+    comments = models.TextField(max_length=1000)
+
+    pass
+
+#
+#
+# class Price(models.Model):
+#     pass
--- app_catalog/services.py
+++ app_catalog/services.py
@@ -1,17 +1,18 @@
 """
 Сервисы для работы с каталогом, товарами, магазинами
 """
-
+from .models import Comments
 
 class ProductServicesMixin:
-    """
-    Класс - примесь для использования сервисов для работы с товарами
-    """
 
     def add_review_to_product(self):
         """
         функция добавления отзыва к товару
         """
+        if self.request.method == 'POST':
+            comments = Comments.objects.get_or_create(comments=self.request.POST['comments'])
+            comments.save()
+            return
 
     def get_viewed_products_history(self):
         """
@@ -22,6 +23,27 @@ class ProductServicesMixin:
         """
         функция добавления товара к списку просмотренных
         """
+
+    def get_list_comments_on_products(self) -> list[str]:
+        """
+        функция получения списка комментариев к продукту
+        """
+        if self.request.method == 'GET':
+            comments = Comments.objects.get(goods=self.request.GET['goods'])
+            return list(comments.objects.get(comments))
+
+    def get_discount_on_products(self):
+        """
+        функция получения скидки к продукту
+        """
+
+    def get_quantity_comments_on_products(self) -> int:
+        """
+        функция получения количество комментариев к продукту
+        """
+        if self.request.method == 'GET':
+            comments = Comments.objects.get(goods=self.request.GET['goods'])
+            return len(comments.objects.get(comments))
 
 
 class ShopServicesMixin:
