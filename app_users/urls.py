3832aa995d54d3c5302a459aaa54d298635f7fe9
Belov <sashabel1985@gmail.com>
2023-05-11 18:21:45 +0300

    попытался сделать интеграцию registr.html выдает ошибку jinga2 посмотри плиз в ту ли я сторону начал делать И вопрос на пятницу - так ли работать с шаблонами?

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,21 +1,21 @@
-# from django.contrib.auth.views import LoginView
-# from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.urls import path
 
-# from .views import (
-#     MyLogoutView,
-#     RegisterView,
-# )
-#
-# app_name = "app_users"
-#
-# urlpatterns = [
-#     # path("register/", RegisterView.as_view(), name="register"),
-#     path(
-#         "login/",
-#         LoginView.as_view(
-#             template_name="app_users/login.html", redirect_authenticated_user=True
-#         ),
-#         name="login",
-#     ),
-#     path("logout/", MyLogoutView.as_view(), name="logout"),
-# ]
+from .views import (
+    # MyLogoutView,
+    RegisterView,
+)
+
+app_name = "app_users"
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    # path(
+    #     "login/",
+    #     LoginView.as_view(
+    #         template_name="app_users/login.html", redirect_authenticated_user=True
+    #     ),
+    #     name="login",
+    # ),
+    # path("logout/", MyLogoutView.as_view(), name="logout"),
+]
--- app_users/views.py
+++ app_users/views.py
@@ -1,23 +1,30 @@
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.views import LogoutView
-# from django.shortcuts import render
-# from django.urls import reverse_lazy
-# from django_jinja.views.generic import CreateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django_jinja.views.generic import CreateView
+
+from app_users.models import User
 
 
-# class RegisterView(CreateView):
-#     form_class = UserCreationForm
-#     template_name = "app_users/register.html"
-#     # success_url = reverse_lazy("app_users:about-me")
-#
-#     def form_valid(self, form):
-#         response = super().form_valid(form)
-#         Profile.objects.create(user=self.object)
-#         username = form.cleaned_data.get("username")
-#         password = form.cleaned_data.get("password1")
-#         user = authenticate(self.request, username=username, password=password)
-#         login(request=self.request, user=user)
-#         return response
+class RegisterView(CreateView):
+    # form_class = UserCreationForm
+    model = User
+    fields = "username", "email", "password"
+    template_name = "registr.html"
+    # success_url = reverse_lazy("app_users:about")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        username = form.cleaned_data.get("name")
+        password = form.cleaned_data.get("password")
+        email = form.cleaned_data.get("email")
+        User.objects.create(user=self.object)
+        user = authenticate(self.request, email=email, password=password)
+        login(request=self.request, user=user)
+        return response
 
 #
 # class MyLogoutView(LogoutView):
