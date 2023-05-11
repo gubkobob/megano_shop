9fe2725bdc741f3ecfd846f3fbc652b60021a2b7
Belov <sashabel1985@gmail.com>
2023-05-11 18:21:45 +0300

    попытался сделать интеграцию registr.html выдает ошибку jinga2 посмотри плиз в ту ли я сторону начал делать И вопрос на пятницу - так ли работать с шаблонами?

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("app_users/", include("app_users.urls")),
+    path("app_users/", include("app_users.urls")),
 ]
