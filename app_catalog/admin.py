6b6ca0ac4014d5946d2e5636126b87d262ee4f24
ignatushenko <ignat-viz@mail.ru>
2023-05-11 09:58:15 +0500

    upgrade_model_shop

--- app_catalog/admin.py
+++ app_catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ShopAdmin(admin.ModelAdmin):
+    """
+    Админ панель модели Shop
+    """
+    list_display = ['name', 'descriptions', 'address', 'phone', 'email', 'image', 'product']
+
+admin.site.register(Shop, ShopAdmin)
