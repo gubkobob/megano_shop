605d0c694340883982c3525deccd2ceb495fb1e0
ignatushenko <ignat-viz@mail.ru>
2023-05-11 07:23:18 +0500

    create_model_shop

--- app_catalog/models.py
+++ app_catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    """Модель магазина"""
+    name = models.CharField(max_length=100)
+    descriptions = models.TextField()
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    image = models.ImageField()
+    product = models.ManyToManyField(to="Products", through='ShopsProducts')
+
+    def __str__(self) -> str:
+        return f"{self.name}"
