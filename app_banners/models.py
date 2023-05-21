c6b3e13d3949776e6017acde425a649b43d5d17c
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2023-05-21 09:37:34 +0300

    Update models.py

--- app_banners/models.py
+++ app_banners/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from app_catalog.models import Product
 class Banner(models.Model):
     name = models.CharField(max_length=255)
-    product=models.ForeignKey(Product,on_delete=models.CASCADE())
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='banners/')
     is_active = models.BooleanField(default=True)
 
