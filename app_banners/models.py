3dfac5c45751e1e3dc5bf01e50ec32ec5ba0e154
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2023-05-21 13:11:34 +0700

    Done

--- app_banners/models.py
+++ app_banners/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+class Banner(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='banners/')
+    is_active = models.BooleanField(default=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+
+class Settings(models.Model):
+    key = models.CharField(max_length=255, unique=True)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.key
