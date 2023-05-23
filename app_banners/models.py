7750c6c3f3b2f8128c8aec9743c13b1b2b0da9a3
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2023-05-23 19:41:33 +0300

    Update models.py

--- app_banners/models.py
+++ app_banners/models.py
@@ -5,7 +5,7 @@ class Banner(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='banners/')
     is_active = models.BooleanField(default=True)
-    position = models.IntegerField()
+    
     
     def __str__(self):
         return self.name
