b5d14f8fb4640ed48f26b4c31a94e45fbeeeee19
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2023-05-23 19:23:56 +0300

    Update models.py

--- app_banners/models.py
+++ app_banners/models.py
@@ -5,15 +5,10 @@ class Banner(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='banners/')
     is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    position = models.IntegerField()
+    
     def __str__(self):
         return self.name
 
 
-class Settings(models.Model):
-    banners = models.ManyToManyField(Banner)
-    cached_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.key
