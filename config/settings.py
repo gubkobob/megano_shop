4fd04492f6d9c3d75d7ec1261a777af81a8b5e31
Максим Семенюк <expmax85@gmail.com>
2023-05-23 23:28:25 +0500

    add static

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ COMPARISON_SESSION_ID = 'comparison'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
 
