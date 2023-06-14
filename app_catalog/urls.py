99aa8f54434f997e3ec7541ab7a8ee21f7a57070
Tatyana Fedutik <tanyushka_s@bk.ru>
2023-06-14 11:12:32 +0300

    1.fixed the link to the Main page

--- app_catalog/urls.py
+++ app_catalog/urls.py
@@ -4,9 +4,6 @@ from .views import *
 app_name = 'appcatalog'
 
 urlpatterns = [
-    # path('', categories_list, name='categories_list'),
-    # path('<category_slug>/', categories_list, name='categories_list_with_products'),
-    # path('<category_slug>/<subcategory_slug>/', categories_list, name='subcategories_list_with_products')
     path('', CategoryView.as_view(), name='catalog'),
     path('product_details/<int:pk>/', ProductCartDetailView.as_view(), name='product_details')
 ]
