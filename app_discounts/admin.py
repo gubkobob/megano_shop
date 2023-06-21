2b023790e4c3572a8a784e09f42e8cc681eefaf1
Александр Малафеевский <sanho50001@bk.ru>
2023-06-21 10:07:33 +0300

    *Пока не рабочий код, для сохранения*

--- app_discounts/admin.py
+++ app_discounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Discount
+from django import forms
 
-# Register your models here.
+
+class DiscountAdminForm(forms.ModelForm):
+    class Meta:
+        model = Discount
+        fields = '__all__'
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    """Админ панель модель Specifications"""
+    list_display = [
+        'product',
+        # 'auto_price',
+        'discount',
+        'start_discount',
+        'end_discount',
+    ]
+    form = DiscountAdminForm
+
+    #
+    # def auto_price(self, obj: Discount):
+    #     return obj.product.price
--- app_discounts/models.py
+++ app_discounts/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from app_catalog.models import ProductInShop, Product
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Discount(models.Model):
+    product = models.ForeignKey(ProductInShop, on_delete=models.CASCADE, verbose_name='Продукт',
+                                related_name='product_discount')
+    # price_product = models.DecimalField(default=0, max_digits=8, decimal_places=2, verbose_name=_('Цена продукта'))
+    discount = models.IntegerField(default=0, verbose_name=_('Скидка'))
+    # Если у продукта есть цена, но нет цены со скидкой, но есть скидка то
+    start_discount = models.DateTimeField()
+    end_discount = models.DateTimeField()
+
+    # Отдельные
+    # - получить все скидки на указанный список товаров или на один товар;
+    # - получить приоритетную скидку на указанный список товаров или на один товар;
+    # - получить скидку на корзину;
+    # - рассчитать цену со скидкой на товар с дополнительным необязательным параметром ― цена товара.
+
+    class Meta:
+        verbose_name = 'Скидка'
+        verbose_name_plural = 'Скидки'
+
+    # def save(
+    #     self, force_insert=False, force_update=False, using=None, update_fields=None
+    # ):
+    #     super(Discount, self).save(
+    #         force_insert=False, force_update=False, using=None, update_fields=None
+    #     )
+    #     if self.price_discount <= 0 and (self.discount > 0):
+    #         self.price_discount = (self.price_product // self.discount)
+    #         return super(Discount, self).save(force_insert, force_update, using, update_fields)
+    #     return
+
+
+class DiscountList(models.Model):
+    pass
+
+
+class DiscountPrior(models.Model):
+    pass
+
+
+class DiscountCart(models.Model):
+    pass
+
+
+class DiscountPrice(models.Model):
+    pass
