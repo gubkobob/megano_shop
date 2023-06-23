e33ec1e3be53fe34b249d8a46ffc2834ccb44e38
ignatushenko <ignat-viz@mail.ru>
2023-06-23 22:03:34 +0500

    detail_product

--- app_catalog/views.py
+++ app_catalog/views.py
@@ -1,8 +1,8 @@
 import datetime
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from app_administrator.models import SettingsModel
-from .models import Product
+from .models import Product, Comments
 from .services import sort_catalog, paginator, filter_catalog
 
 
@@ -40,29 +40,31 @@ class CategoryView(ListView):
                           )
 
 
-def product_details(request, pk):
-    product = Product.objects.get(pk=pk)
-    product.last_visit = datetime.datetime.now()
-    product.save()
-    count_viewed = getattr(SettingsModel.objects.first(), 'count_viewed')
-    recently_viewed_products = None
-    product_id = pk
+class ProductCartDetailView(DetailView):
+    model = Product
+    template_name = 'app_catalog/product_details.jinja2'
 
-    if 'recently_viewed' in request.session:
-        if product_id in request.session['recently_viewed']:
-            request.session['recently_viewed'].remove(product_id)
-        request.session['recently_viewed'].insert(0, product_id)
-        recently_viewed_products = Product.objects.filter(pk__in=request.session['recently_viewed']).order_by(
-            '-last_visit')
-        if len(request.session['recently_viewed']) > count_viewed:
-            request.session['recently_viewed'].pop(-1)
-    else:
-        request.session['recently_viewed'] = [product_id]
-    request.session.modified = True
-    context = {
-        'product': product,
-        'recently_viewed_products': recently_viewed_products
-    }
-    return render(request, 'app_catalog/product_details.jinja2', context)
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        product = Product.objects.get(pk=self.kwargs.get('pk'))
+        comments = Comments.objects.filter(goods=product)
+        context['comments'] = comments
+        return context
+
+    def post(self, request, pk=None):
+        if request.method == "POST":
+            text_comment = request.POST.get('review')
+            user = request.user
+            product = Product.objects.get(pk=pk)
+            new_comment = Comments.objects.create(user=user, comment=text_comment, goods = product)
+            new_comment.save()
+            comments = Comments.objects.filter(goods=product)
+            return render(request, 'app_catalog/product_details.jinja2',
+                          context={
+                              'comments': comments,
+                              'product': product,
+                          }
+                          )
 
 
