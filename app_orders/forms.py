ada24f0e090a1dfc59c5a80684511ea3e30dd7bc
Tatyana Fedutik <tanyushka_s@bk.ru>
2023-07-03 16:47:03 +0300

    добавлены административные настройки для модели заказов; создана модель заказов

---
+++ app_orders/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ("full_name", "phone_number", "email", "city", "address", "buying_type", "payment", "comment", "status")
