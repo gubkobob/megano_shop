82cb5b07aa2db9ef5591eedb891a20b4979acf93
Belov <sashabel1985@gmail.com>
2023-07-10 13:36:52 +0300

    сделано детальная страница и список заказов

--- app_orders/urls.py
+++ app_orders/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CheckoutOrderView, CreateOrderView
+from .views import CheckoutOrderView, CreateOrderView, OrderDetailView, OrdersListView
 
 app_name = 'apporders'
 
 urlpatterns = [
     path('checkout/', CheckoutOrderView.as_view(), name='checkout_order'),
-    path('create/', CreateOrderView.as_view(), name='create_order')
+    path('create/', CreateOrderView.as_view(), name='create_order'),
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order_details"),
+    path('history/', OrdersListView.as_view(), name='orders_history'),
 ]
