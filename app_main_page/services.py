d5346f82425e5c8392759609f59390cbb7960b51
Tatyana Fedutik <tanyushka_s@bk.ru>
2023-07-15 04:23:47 +0300

    Отфильтрованы 'популярные товары' на главной начиная от наиболее продаваемых товаров

--- app_main_page/services.py
+++ app_main_page/services.py
@@ -1,8 +1,7 @@
 from django.db.models import Sum
 from datetime import datetime
 from app_administrator.models import SettingsModel
-from django.db.models import Min
-from app_catalog.models import Product, ProductInShop
+from app_catalog.models import ProductInShop
 
 
 class Limit:
@@ -41,8 +40,8 @@ def get_products_limited(product_day):
 
 
 def get_top_products():
-    """функция отображения популярных товаров на главной"""
-    # top_products = Product.objects.annotate(quantity_sum=Sum('order_items__quantity')).order_by('-quantity_sum')[:8]
+    """функция отображения популярных товаров на главной (фильтр от наиболее продаваемых)"""
     value_popular_products = getattr(SettingsModel.objects.first(), 'popular_products')
-    top_products = ProductInShop.objects.all().order_by('price')[:value_popular_products]
+    top_products = ProductInShop.objects.annotate(quantity_sum=Sum('order_items__quantity')).order_by('-quantity_sum')[
+                   :value_popular_products]
     return top_products
