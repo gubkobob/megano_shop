ff4c79dcd5fe9eaad2199d890678b31082fb81e7
Belov <sashabel1985@gmail.com>
2023-07-17 16:11:16 +0300

    изменил celery.py чтобы docker compose корректно работал

--- config/celery.py
+++ config/celery.py
@@ -5,8 +5,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# app = Celery('config', backend='rpc://', broker='amqp://guest:guest@rabbit:5672')
-app = Celery('config', backend='rpc://', broker='amqp://')
+app = Celery('config', backend='rpc://', broker='amqp://guest:guest@rabbit:5672')
+# app = Celery('config', backend='rpc://', broker='amqp://')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
